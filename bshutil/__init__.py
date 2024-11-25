a857b31e1d0e6119578a9ae9c10cef4179480fa2
KDUser <128939316+KDUser12@users.noreply.github.com>
2024-11-25 22:14:02 +0100

    bshutil: add basic script

---
+++ bshutil/__init__.py
@@ -0,0 +1,10 @@
+# (c) 2024 KDUser12 on GitHub.
+# Licensed under the MIT License.
+
+"""bshutil (backup shell utilities)
+bshutil is a program using the shutil library to back up files and folders
+simply, reliably and quickly. It allows creating backup copies of any file
+or directory, while providing flexible options for customizing backups.
+"""
+
+__version__ = "1.2.0-dev1"
